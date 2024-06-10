8f69c26ac1c6ebc6d22d18eee5a323adbf676747
maksarovd <bondspear2020@gmail.com>
2024-06-10 03:58:24 +0300

    place: CommentController.php action: Fix return type of view

--- laravel/app/Http/Controllers/CommentController.php
+++ laravel/app/Http/Controllers/CommentController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\{Request, JsonResponse, RedirectResponse};
-use Illuminate\View\View;
+use Illuminate\Contracts\View\View;
 use App\Models\{Comment, File};
 use App\Http\Requests\{ValidateUploadRequest, CheckRequest};
 use App\Services\CommentService;
